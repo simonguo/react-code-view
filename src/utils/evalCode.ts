de155a48ee75270c11b64cee01d42fba9c44c450
simonguo <simonguo.2009@gmail.com>
2022-08-01 12:12:13 +0800

    improve(transform): use sucrase instead of @swc/wasm-web to improve transcoding performance

---
+++ src/utils/evalCode.ts
@@ -0,0 +1,7 @@
+const evalCode = (code, scope) => {
+  const scopeKeys = Object.keys(scope);
+  const scopeValues = scopeKeys.map(key => scope[key]);
+  return new Function(...scopeKeys, code)(...scopeValues);
+};
+
+export default evalCode;
